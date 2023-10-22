57af9f80193b1f656f428ece32ca5952bc746216
gagan11122 <147685895+gagan11122@users.noreply.github.com>
2023-10-22 23:51:41 +0530

    Create bootstrap.jsx

---
+++ bootstrap.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+function bootstrap(){
+    return(
+        <>
+        <h1 className="text-primary text-center">Bootstrap in react</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Cumque veniam, dolorum quasi dolore totam delectus voluptatibus qui autem pariatur,
+             eum ad enim aliquid repudiandae veritatis consequuntur ratione quos, doloribus nihil?
+             </p>
+
+        </>
+    )
+}
+export default bootstrap;
